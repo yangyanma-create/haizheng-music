bf39960394aae33c5f65f7f1029158b03281bf56
chiyangma <chiyangma@CHIYANGdeMacBook-Pro.local>
2026-06-13 00:20:59 +0800

    feat: switch to Noto Serif TC for elegant Chinese typography

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from 'next'
-import { Playfair_Display, Ma_Shan_Zheng } from 'next/font/google'
+import { Playfair_Display, Noto_Serif_TC } from 'next/font/google'
 import './globals.css'
 
 const playfair = Playfair_Display({
@@ -7,9 +7,9 @@ const playfair = Playfair_Display({
   variable: '--font-playfair',
 })
 
-const zcool = Ma_Shan_Zheng({
+const zcool = Noto_Serif_TC({
   subsets: ['latin'],
-  weight: '400',
+  weight: ['300', '400'],
   variable: '--font-zcool',
 })
 
